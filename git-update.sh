5be51863f1888ef60b5f4297828055d46577fa0a
Telmo Brugnara <tbrugz@gmail.com>
2017-01-13 11:48:34 -0200

    build: atualizando deploy scripts

--- git-update.sh
+++ git-update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # see http://stackoverflow.com/a/31186538/616413
 
-REPO_DIR="themes/hugo-type-theme/"
-REPO_URL="https://github.com/digitalcraftsman/hugo-type-theme.git"
+#REPO_DIR="themes/hugo-type-theme/"
+#REPO_URL="https://github.com/digitalcraftsman/hugo-type-theme.git"
+
+REPO_DIR="themes/hugo-lithium-theme/"
+REPO_URL="https://github.com/yihui/hugo-lithium-theme.git"
 
 git -C $REPO_DIR pull || git clone $REPO_URL $REPO_DIR
